4e3c951ecced13ae5ed53930f5ea656add220e81
bea4dev <bea0224@outlook.jp>
2024-06-08 23:07:34 +0900

    commit

--- src/main/kotlin/level/generator/SnowLandGenerator.kt
+++ src/main/kotlin/level/generator/SnowLandGenerator.kt
@@ -1,5 +1,7 @@
 package level.generator
 
+import de.articdive.jnoise.core.api.functions.Interpolation
+import de.articdive.jnoise.generators.noise_parameters.fade_functions.FadeFunction
 import de.articdive.jnoise.generators.noisegen.opensimplex.FastSimplexNoiseGenerator
 import de.articdive.jnoise.pipeline.JNoise
 import level.biome.SnowDawnBiomes
@@ -41,20 +43,50 @@ class SnowLandGenerator(seed: Long): Generator {
     }
     private val caveNoise1 = ThreadLocal.withInitial {
         JNoise.newBuilder()
-            .fastSimplex(FastSimplexNoiseGenerator.newBuilder().setSeed(seed + 500).build())
-            .scale(0.01)
+            .perlin(seed + 500, Interpolation.LINEAR, FadeFunction.NONE)
+            .scale(0.02)
             .build()
     }
     private val caveNoise2 = ThreadLocal.withInitial {
         JNoise.newBuilder()
-            .fastSimplex(FastSimplexNoiseGenerator.newBuilder().setSeed(seed + 600).build())
-            .scale(0.02)
+            .perlin(seed + 600, Interpolation.LINEAR, FadeFunction.NONE)
+            .scale(0.03)
             .build()
     }
     private val caveNoise3 = ThreadLocal.withInitial {
         JNoise.newBuilder()
             .fastSimplex(FastSimplexNoiseGenerator.newBuilder().setSeed(seed + 700).build())
             .scale(0.05)
+            .build()
+    }
+    private val caveNoise4 = ThreadLocal.withInitial {
+        JNoise.newBuilder()
+            .perlin(seed + 800, Interpolation.COSINE, FadeFunction.NONE)
+            .scale(0.02)
+            .build()
+    }
+    private val caveNoise5 = ThreadLocal.withInitial {
+        JNoise.newBuilder()
+            .perlin(seed + 900, Interpolation.LINEAR, FadeFunction.NONE)
+            .scale(0.04)
+            .build()
+    }
+    private val caveNoise6 = ThreadLocal.withInitial {
+        JNoise.newBuilder()
+            .fastSimplex(FastSimplexNoiseGenerator.newBuilder().setSeed(seed + 1000).build())
+            .scale(0.06)
+            .build()
+    }
+    private val iceNoise1 = ThreadLocal.withInitial {
+        JNoise.newBuilder()
+            .fastSimplex(FastSimplexNoiseGenerator.newBuilder().setSeed(seed + 1100).build())
+            .scale(0.01)
+            .build()
+    }
+    private val iceNoise2 = ThreadLocal.withInitial {
+        JNoise.newBuilder()
+            .fastSimplex(FastSimplexNoiseGenerator.newBuilder().setSeed(seed + 1200).build())
+            .scale(0.02)
             .build()
     }
 
@@ -68,13 +100,19 @@ class SnowLandGenerator(seed: Long): Generator {
         val caveNoise1 = caveNoise1.get()
         val caveNoise2 = caveNoise2.get()
         val caveNoise3 = caveNoise3.get()
+        val caveNoise4 = caveNoise4.get()
+        val caveNoise5 = caveNoise5.get()
+        val caveNoise6 = caveNoise6.get()
+        val iceNoise1 = iceNoise1.get()
+        val iceNoise2 = iceNoise2.get()
 
         val start = unit.absoluteStart()
         for (x in 0..<unit.size().x().toInt()) {
             for (z in 0..<unit.size().z().toInt()) {
                 val bottom = start.add(x.toDouble(), 0.0, z.toDouble())
 
-                var landHeight = baseNoise.evaluateNoise(bottom.x(), bottom.z()) * 16 + 64
+                val lowerLandHeight = baseNoise.evaluateNoise(bottom.x(), bottom.z()) * 16 + 64
+                var landHeight = lowerLandHeight
 
                 val shapeValue1 = shapeNoise1.evaluateNoise(bottom.x(), bottom.z()) * 8 + 8
                 if (shapeValue1 > 8) {
@@ -97,19 +135,34 @@ class SnowLandGenerator(seed: Long): Generator {
                 for (y in 0..<unit.size().y().toInt()) {
                     val position = start.add(x.toDouble(), y.toDouble(), z.toDouble())
 
-                    val caveValue = caveNoise1.evaluateNoise(position.x(), position.y(), position.z()) +
+                    val caveValue1 = caveNoise1.evaluateNoise(position.x(), position.y(), position.z()) +
                             caveNoise2.evaluateNoise(position.x(), position.y(), position.z()) +
                             caveNoise3.evaluateNoise(position.x(), position.y(), position.z()) / 2
 
-                    val isCave = if (position.y() < landHeight) {
-                        caveValue > 1.2
+                    val caveValue2 = caveNoise4.evaluateNoise(position.x(), position.y(), position.z()) +
+                            caveNoise5.evaluateNoise(position.x(), position.y(), position.z()) +
+                            caveNoise6.evaluateNoise(position.x(), position.y(), position.z()) / 3
+
+                    val isCave = if (position.y() < lowerLandHeight) {
+                        caveValue1 > 0.8 || caveValue2 > 0.8 || caveValue1 < -0.8 || caveValue2 < -0.8
                     } else {
-                        caveValue > 1.5
+                        caveValue1 > 1.3 || caveValue2 > 1.0 || caveValue1 < -1.3 || caveValue2 < -1.0
                     }
+
+                    val iceValue = iceNoise1.evaluateNoise(position.x(), position.y(), position.z()) +
+                            iceNoise2.evaluateNoise(position.x(), position.y(), position.z()) / 2
 
                     if (position.y() < landHeight.toInt() - 1) {
                         if (!isCave) {
-                            unit.modifier().setBlock(position, Block.STONE)
+                            if (iceValue > 0.8) {
+                                if (iceValue > 1.0) {
+                                    unit.modifier().setBlock(position, Block.PACKED_ICE)
+                                } else {
+                                    unit.modifier().setBlock(position, Block.ICE)
+                                }
+                            } else {
+                                unit.modifier().setBlock(position, Block.STONE)
+                            }
                         }
                     }
 
